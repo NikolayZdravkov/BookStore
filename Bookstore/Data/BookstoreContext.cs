f19f3d4c3d014b9159c2ea930d817775c681f502
niketzaBG <zdravkovnikolay5@gmail.com>
2024-11-24 20:41:08 +0200

    Added BooksController and initial migration

---
+++ Bookstore/Data/BookstoreContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BookStore.Models;
+
+namespace Bookstore.Data
+{
+    public class BookstoreContext : DbContext
+    {
+        public BookstoreContext (DbContextOptions<BookstoreContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<BookStore.Models.Book> Book { get; set; } = default!;
+    }
+}
