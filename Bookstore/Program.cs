0052aeff49e4d273ce64f9242ad1bbbc28773a71
niketzaBG <zdravkovnikolay5@gmail.com>
2024-11-24 22:57:48 +0200

    Added fontawesome and data seed for Books

--- Bookstore/Program.cs
+++ Bookstore/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Bookstore.Data;
+using BookStore.Models;
 namespace Bookstore
 {
     public class Program
@@ -11,10 +12,31 @@ namespace Bookstore
             builder.Services.AddDbContext<BookstoreContext>(options =>
                 options.UseSqlServer(builder.Configuration.GetConnectionString("BookstoreContext") ?? throw new InvalidOperationException("Connection string 'BookstoreContext' not found.")));
 
+            
+
             // Add services to the container.
             builder.Services.AddControllersWithViews();
 
             var app = builder.Build();
+
+            using (var scope = app.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    // Initialize Roles and Users
+                    //UserRoleInitializer.InitializeAsync(services).Wait();
+
+                    // Seed book data
+                    SeedData.Initialize(services);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred while attempting to seed the database.");
+                }
+            }
 
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
