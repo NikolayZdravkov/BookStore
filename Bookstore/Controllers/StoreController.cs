2c93ef11f5023a77810586f9eb4d5b8378999307
niketzaBG <zdravkovnikolay5@gmail.com>
2024-11-26 21:44:15 +0200

    Added Store view

---
+++ Bookstore/Controllers/StoreController.cs
@@ -0,0 +1,36 @@
+﻿using Bookstore.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bookstore.Controllers
+{
+    public class StoreController : Controller
+    {
+
+        private readonly BookstoreContext _context;
+
+        public StoreController(BookstoreContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Books.ToListAsync());
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var book = await _context.Books.FindAsync(id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            return View(book);
+        }
+    }
+}
