f2f1f481a9442a5ded598a285b9440dfc2ee4d2f
niketzaBG <zdravkovnikolay5@gmail.com>
2024-11-30 16:46:40 +0200

    Added Checkout method in OrderController

--- Bookstore/Controllers/OrderController.cs
+++ Bookstore/Controllers/OrderController.cs
@@ -21,6 +21,27 @@ namespace Bookstore.Controllers
             return View();
         }
 
+        [HttpPost]
+        public IActionResult Checkout(Order order)
+        {
+            var cartItems = _cart.GetAllCartItems();
+            _cart.CartItems = cartItems;
+
+            if (_cart.CartItems.Count == 0)
+            {
+                ModelState.AddModelError("", "Cart is empty, please add a book first.");
+            }
+
+            if (ModelState.IsValid)
+            {
+                CreateOrder(order);
+                _cart.ClearCart();
+                return View("CheckoutComplete", order);
+            }
+
+            return View(order);
+        }
+
         public void CreateOrder(Order order)
         {
             order.OrderPlaced = DateTime.Now;
